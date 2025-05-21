f3954654463171dbe06cd657bca196d713c5579a
Alon Goddarde <alongoddard1@gmail.com>
2025-05-21 15:09:40 +0300

    Fix

--- app/components/songLineDisplays/SongLine.tsx
+++ app/components/songLineDisplays/SongLine.tsx
@@ -80,24 +80,11 @@ const SongLine: React.FC<SongLineProps> = ({ details, onClick }) => {
           />
         </button>
         <button className="action-button checkbox-container">
-          <svg
+          <MuzaIcon
+            iconName="Checkbox"
             className="checkbox-icon"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="3"
-              y="3"
-              width="18"
-              height="18"
-              rx="2"
-              stroke="currentColor"
-              strokeWidth="2"
-            />
-          </svg>
+            svgStyle={{ width: "16px", height: "16px" }}
+          />
         </button>
       </div>
     );
